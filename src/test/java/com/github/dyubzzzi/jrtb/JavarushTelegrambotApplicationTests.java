c217c2cfa1aa90c9d0b9b701801123ab09d2091d
Alex <alex.learn.material@gmail.com>
2023-04-09 15:56:39 +0300

    add springBoot

---
+++ src/test/java/com/github/dyubzzzi/jrtb/JavarushTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dyubzzzi.jrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
